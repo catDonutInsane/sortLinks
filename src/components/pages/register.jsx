7fab440510f04c4009835947f620b1859640e597
catDonutInsane <124159300+catDonutInsane@users.noreply.github.com>
2024-02-10 22:41:31 +0300

    Update register.jsx

--- src/components/pages/register.jsx
+++ src/components/pages/register.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 export const Register = () =>{
     const [name, setName] = useState("")
     const [pass, setPass] = useState("")
-    const [data, setData] = useState(null)
+    const [data, setData] = useState("")
     const regHandler = async () =>{
         let data = await fetch(`https://front-test.hex.team/api/register?username=${name}&password=${pass}`,
                 {
@@ -13,8 +13,8 @@ export const Register = () =>{
                     }
                 }
                 )
-                setData(data)
-        console.log(data)
+        let result = data.statusText==="OK"?"вы зареганы":`полльзователь с именем ${name} уже существует`
+                setData(result)
     }
 
     return(
@@ -22,7 +22,7 @@ export const Register = () =>{
             <input  onChange={(e)=>setName(e.currentTarget.value)} type="text" placeholder="Имя" value={name}/>
             <input onChange={(e)=>setPass(e.currentTarget.value)} type="password" placeholder="пароль" value={pass}/>
             <button onClick={regHandler}>Зарегистрироваться</button>
-            {data?.statusText=== 'OK'?<div>вы зареганы</div>:<div>{`полльзователь с именем ${name} уже существует`}</div>}
+            {data}
         </div>
     )
 }
